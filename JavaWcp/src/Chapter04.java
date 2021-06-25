77f39ac13ccaf39a05810a519a88936067fcacb5
hirotomoriyama-g <hiroto.moriyama7204@gmail.com>
2021-06-25 22:56:40 +0900

    変数と定数の使用（確認問題）

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	
+	public static void main(String... args) {
+		String name = "Hiroto";
+		System.out.println(name);
+		
+		name = "Moriyama";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
