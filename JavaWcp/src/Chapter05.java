2bcce983a6bbe686fa0ed2e843e86c278014623a
hirotomoriyama-g <hiroto.moriyama7204@gmail.com>
2021-06-27 18:07:03 +0900

    型、配列の使用（確認問題）

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+	
+	public static void main(String[] args) {
+		long long1 = 7485L;
+		System.out.println(long1);
+		double double1 = 4343.75;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 0.3435f;
+		System.out.println(float1);
+		Integer integer1 = 564;
+		System.out.println(integer1);
+		Character character1 = 'あ';
+		System.out.println(character1);
+		
+		int[] array1 = { 10, 20, 30, 40, 50 };
+		int int2 = array1[3];
+		System.out.println(int2);
+	}
+
+}
